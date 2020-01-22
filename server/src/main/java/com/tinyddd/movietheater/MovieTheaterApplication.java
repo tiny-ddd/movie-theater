253078230740f26db1981c8615991c1366f3fc0a
Paige Ruppel <paige.ruppel@gmail.com>
2020-01-22 17:54:17 -0500

    converted to support multi-module build - added server module

--- server/src/main/java/com/tinyddd/movietheater/MovieTheaterApplication.java
+++ server/src/main/java/com/tinyddd/movietheater/MovieTheaterApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MovietheaterApplication {
+public class MovieTheaterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MovietheaterApplication.class, args);
+		SpringApplication.run(MovieTheaterApplication.class, args);
 	}
 
 }
