86a249ed00556ee9795b8f8d57cac587f6c3dcd9
Paige Ruppel <paige.ruppel@gmail.com>
2020-01-22 17:27:51 -0500

    First commit after initialization with spring initializr - before refactoring to support multi-module build

---
+++ src/main/java/com/tinyddd/movietheater/MovietheaterApplication.java
@@ -0,0 +1,13 @@
+package com.tinyddd.movietheater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovietheaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovietheaterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tinyddd/movietheater/MovietheaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinyddd.movietheater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovietheaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
